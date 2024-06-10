01035bb0a7579f64df41111f3d14ba59cc3d5346
Uditya <uditya204@gmail.com>
2024-06-10 12:10:33 +0530

    Added list comprehension

--- 6. array.py
+++ 6. array.py
@@ -30,3 +30,23 @@ a= array.array("i",[2,1,4,3])
 # lst= list(a)
 # lst.sort()
 # print(lst)
+
+#---------------
+#LIST COMPREHENSION
+
+#Method 1 - Basic method to add all the elements of list smaller than a
+# def separate(lst,a):
+#     result = []
+#     for i in lst:
+#         if i < a:
+#             result.append(i)
+#     return result
+
+# print(separate([1,2,3,4],3))
+
+#Method 2 - shortcut (list comprehension method)
+def separate(lst,a):
+    result = [i for i in lst if i<a]
+    return result
+
+print(separate([1,2,3,4],3))
