caf28167fde58051fb8574070b89a62a05c02e38
Uditya <uditya204@gmail.com>
2024-06-11 10:53:54 +0530

    Added list comprehension

---
+++ 6.2 List comprehension.py
@@ -0,0 +1,22 @@
+# IN LIST
+
+# EXAMPLE 1 - Combining 2 lists
+# l1=[1,2,3,4,5]
+# l2 = ["udi", "vidu", "ash","adi", "jig"]
+# d3= dict(zip(l1,l2))
+# print(d3)
+
+#EXAMPLE 2 - make separate lists containing even and odd elements of a list
+# lst=[1,2,3,4]
+# evenlst=[x for x in lst if x%2==0]
+# oddlst=[x for x in lst if x%2!=0]
+
+# print("even list" , evenlst)
+# print("odd list", oddlst)
+
+#----------------------------------------------------
+# IN DICTIONARY
+#Exchange key value postions
+# # d1 = {101:"gfg", 103:"pracctise", 102: "ide"}
+# # d2 = {k:v for (v,k) in d1.items()}
+# print(d2)
