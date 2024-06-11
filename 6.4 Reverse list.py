4a93a251ca222980999d73288a19936f393e9458
Uditya <uditya204@gmail.com>
2024-06-11 12:48:09 +0530

    Added reverse list file

---
+++ 6.4 Reverse list.py
@@ -0,0 +1,21 @@
+"""
+1. in odd length list star and end index pointers will overlap(becomes same).
+2. in even length list , in final iteration, startindex moves ahead of endindex.
+"""
+def rev(list):
+    startindex=0
+    endindex=len(list)-1
+
+    while startindex<endindex:
+
+        list[startindex], list[endindex]=list[endindex], list[startindex]
+
+        startindex +=1
+        endindex -=1
+    
+    return list
+
+l=[10,20,30,40]
+l2=["ab","bc","cd"]
+print(rev(l))
+print(rev(l2))
