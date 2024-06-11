194b8667b8471a32bc6d0895872e1e094ab7055c
Uditya <uditya204@gmail.com>
2024-06-11 11:55:14 +0530

    Added line

--- 6.3 check list sorted.py
+++ 6.3 check list sorted.py
@@ -1,4 +1,4 @@
-#Method 1- With built-in function
+#Method 1- With built-in function (sort function does more work)
 # def checksort(list):
 #     newlist= sorted(list)
 #     if newlist == list:
@@ -6,19 +6,19 @@
 #     else:
 #         return False
 
-#Method 2 - Without built-in function
-# def checksort(list):
-#     if len(list) <=1:
-#         return True
+#Method 2 - Without built-in function (Prefered)
+def checksort(list):
+    if len(list) <=1:
+        return True
     
-#     prev=list[0]
-#     for i in list[1:]:            
-#         if i< prev:
-#             return False
+    prev=list[0]
+    for i in list[1:]:            
+        if i< prev:
+            return False
         
-#         prev = i
+        prev = i
     
-#     return True
+    return True
 
 l=[1,2,4,3]
 if checksort(l):
