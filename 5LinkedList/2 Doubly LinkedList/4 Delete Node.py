3dd8d1be52376bfe10705d0c22214305b610d1fa
Uditya <uditya204@gmail.com>
2024-07-26 09:00:12 +0530

    changes

--- 5LinkedList/2 Doubly LinkedList/4 Delete Node.py
+++ 5LinkedList/2 Doubly LinkedList/4 Delete Node.py
@@ -1,15 +1,27 @@
 class Node:
-    def __init__(self,data):
-        self.data= data 
-        self.prev = None
+    def __init__(self, data):
+        self.data = data
         self.next = None
+        self.prev = None
 
 def show(head):
     current = head
-    while current != None:
+    while current  != None :
         print(current.data, end= "-> ")
         current = current.next
     print("None")
+
+def deleteFromBeg(head):
+    if head == None:
+        return None
+    
+    elif head.next ==None:
+        return None
+    
+    else:
+        head = head.next
+        head.prev = None
+        return head
 
 def DeleteFromEnd(head):
     if head == None:
@@ -24,12 +36,12 @@ def DeleteFromEnd(head):
     current.next = None
     return head
 
-
-
+#Creating Nodes
 head = Node(10)
 node2 = Node(20)
 node3 = Node(30)
 
+#Linking Nodes
 head.next = node2
 
 node2.prev = head
@@ -37,6 +49,11 @@ node2.next = node3
 
 node3.prev = node2
 
+show(head)
+
+#------------------
+#Delete From Beginning of List
+head = deleteFromBeg(head)
 show(head)
 
 #=============
