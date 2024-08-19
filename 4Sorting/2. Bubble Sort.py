b8685eaa8856abb985b6a00cc9a73d2d3f4e48fc
Uditya <uditya204@gmail.com>
2024-08-19 21:36:40 +0530

    changes

--- 4Sorting/2. Bubble Sort.py
+++ 4Sorting/2. Bubble Sort.py
@@ -1,10 +1,14 @@
-lst=[13,46,24,50,20,9]
-print("Original List: ", lst)
-n=len(lst)
+def BubbleSort(arr):
+    n = len(arr)
 
-for i in range(n-1,0,-1):   # i -> n-1 to 1
-    for j in range(0,i):
-        if lst[j]>lst[j+1]:
-            lst[j],lst[j+1] = lst[j+1], lst[j]
+    for i in range(n-1, 0, -1):    # i -> n-1 to 1
 
-print("Sorted List: ", lst)
+        for j in range(0, i):      # j -> 0 to i-1 , as we are comparing element with the adjacent next element. If we go till "i" we will exceed list index.
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+    return arr
+
+arr = [1,6,2,4,9]
+print("Original List: ", arr)
+print("Sorted List: ", BubbleSort(arr))
