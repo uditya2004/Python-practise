cc85415886da0352cb6db4688ae08b09a0171e04
Uditya <uditya204@gmail.com>
2024-09-09 18:53:16 +0530

    changes

--- 3BasicRecursion/1. basics.py
+++ 3BasicRecursion/1. basics.py
@@ -5,6 +5,7 @@ Step 1 :- Find out the main task that the function does in one call first
 Step 2 :- Then call the same function at the step you think -> Task repeats here after.
 Step 3 :- Think if you have to return the function call line or not -> (Imagine: if you are asking the function for the answer then return.)
 Step 4 :- Then Write the BASE CASE condition
+Step 5 :- Check By doing a Dry Run of Test Cases
 """
 #Printing 1 to 3 using recursive function
 # def show(count):
