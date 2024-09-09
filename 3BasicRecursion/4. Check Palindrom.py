cd43a7fcad8c740d75050a7650f4991030e508d0
Uditya <uditya204@gmail.com>
2024-09-09 18:59:00 +0530

    changes

--- 3BasicRecursion/4. Check Palindrom.py
+++ 3BasicRecursion/4. Check Palindrom.py
@@ -2,44 +2,46 @@
 # TC: O(N)
 # SC: O(N)  {Using temp array "temp"}
 
-# def check_pal(str):
-#     temp = ""
-#     for i in range(len(str)-1, -1, -1):
-#         temp = temp + str[i]
+def check_pal(str):
+    temp = ""
+    for i in range(len(str)-1, -1, -1):
+        temp = temp + str[i]
 
-#     if temp == str:
-#         print("True")
-#     else:
-#         print("False")
+    if temp == str:
+        print("True")
+    else:
+        print("False")
 
 
-# str1 = "ABCDCBA"
-# check_pal(str1)
+str1 = "ABCDCBA"
+check_pal(str1)
 
 #============================
 #Method 2: Using 2 Pointer (Run a for loop till half the length of the string in order to check the first and last character of the string.)
 # TC: O(N/2) = O(N)
 # SC: O(1)  {Using temp array "temp"}
 
-# def check_pal(str, low , high):
-#     mid = (low + high) // 2
+def check_pal(str, low , high):
+    mid = (low + high) // 2
 
-#     while low<mid or high>mid:
-#         if str[low] != str[high]:
-#             return False
-#         else:
-#             low = low + 1
-#             high =high - 1
+    while low<mid or high>mid:
+        if str[low] != str[high]:
+            return False
+        else:
+            low = low + 1
+            high =high - 1
     
-#     return True        
+    return True        
 
 
-# str1 = "ABCDCBA"
-# print(check_pal(str1, 0, len(str1) - 1))
+str1 = "ABCDCBA"
+print(check_pal(str1, 0, len(str1) - 1))
 
 #===========================================
-# Method 3: Functional Recursion - Using 2 Pointer variable
- 
+# Method 3: Recursion:- Functional Recursion - Using 2 Pointer variable
+ # TC:- O(N/2)
+ # SC:- O(N/2)   {N/2 function call will be waiting}
+
 def check_pal(str, low , high):
     if low>=high:                 #BASE CASE
         return True
@@ -54,7 +56,7 @@ str1 = "ABCDCBA"
 print(check_pal(str1, 0, len(str1) - 1))
 
 #------------------------------------------
-# Method 3: Functional Recursion - Using 1 Pointer variable
+# Method 3: Recursion:- Functional Recursion - Using 1 Pointer variable
 
 def check_pal(n, str, low):
     if low>=n//2:                  #BASE CASE
