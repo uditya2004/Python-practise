be06de28dbe428ff35124034bae6ea2a600978ac
Uditya <uditya204@gmail.com>
2024-09-11 14:42:05 +0530

    changes

--- DSA/3BasicRecursion/4. Check Palindrom.py
+++ DSA/3BasicRecursion/4. Check Palindrom.py
@@ -70,3 +70,39 @@ def check_pal(n, str, low):
 
 str1 = "ABCDCBA"
 print(check_pal(len(str1), str1, 0))
+
+#===============================================
+"""
+Ques:- 
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+"""
+
+
+def isPalindrome(s):
+    
+    
+    left = 0
+    right = len(s)-1
+
+    while left<right:
+        if not s[left].isalnum():
+            left +=1
+
+        elif not s[right].isalnum(): 
+            right -= 1
+
+        elif s[left].lower() != s[right].lower():
+            return False
+            
+        else:
+            left +=1
+            right -=1
+    
+    return True
+
+    
+
+s = "race a car"
+print(isPalindrome(s))
