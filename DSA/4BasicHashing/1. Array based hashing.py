5d55b456f113ca07dd40f9c0f9bdaeaebbb5bc0b
Uditya <uditya204@gmail.com>
2024-09-13 09:28:53 +0530

    changes

--- DSA/4BasicHashing/1. Array based hashing.py
+++ DSA/4BasicHashing/1. Array based hashing.py
@@ -21,12 +21,45 @@ Ques 1:- Given an array of integers: [1, 2, 1, 3, 2] and we are given some queri
 # Method: Array-Based Hashing (Direct Mapping)
 
 
-arr = [1, 3, 2, 5, 3]
-max_value = max(arr)
+# arr = [1, 3, 2, 5, 3]
+# max_value = max(arr)
 
-hash_table = [0] * (max_value + 1)  # Create array of size (max_value + 1)  (i.e [0,0,0,0,0,0])
+# hash_table = [0] * (max_value + 1)  # Create array of size (max_value + 1)  (i.e [0,0,0,0,0,0])
 
-for num in arr:
-    hash_table[num] += 1  # Store frequency at the number's index
+# for num in arr:
+#     hash_table[num] += 1  # Store frequency at the number's index
 
-print(hash_table)  # Output: [0, 1, 1, 2, 0, 1]
+# print(hash_table)  # Output: [0, 1, 1, 2, 0, 1]
+
+
+#========================================
+#CHARACTER HASHING
+"""
+Ques 2: you are given the string "abcdabefc" and need to answer how many times certain characters appear.
+
+Concept:-
+- We use ASCII values of "a to z" and map "a to index 0" and "z to index 25" . So we make an array of length 26.
+
+"""
+
+def build_hash_table(str1, char_hash_table):
+    for i in str1:
+        hash_value = ord(i)-ord('a')         #The ord() function returns the ASCII value of a character. 
+        char_hash_table[hash_value] +=1
+    
+    return char_hash_table
+
+def fetching(char_hash_table, key):
+    hash_value = ord(key)-ord('a')
+    return char_hash_table[hash_value]
+
+# Sample string and queries
+char_hash_table = [0] * 26        
+str1 = "abcdabefc"
+char_queries = ['a', 'g', 'h', 'b', 'c']
+
+char_hash_table = build_hash_table(str1, char_hash_table)
+
+for query in char_queries:
+    print(f"{query} occurs {fetching(char_hash_table, query)} times")
+
--- DSA/4BasicHashing/tempCodeRunnerFile.py
+++ DSA/4BasicHashing/tempCodeRunnerFile.py
@@ -1,42 +1,8 @@
-class MyHash:
-    def __init__(self, n):
-        self.bucket_size = n
-        self.hash_table = [[] for _ in range(self.bucket_size)]
-    
-    def insert(self, value):
-        hash_value = value %  self.bucket_size
-        self.hash_table[hash_value].append(value)
-    
-    def search(self, key):
-        hash_value = key %  self.bucket_size
-        if key in self.hash_table[hash_value]:
-            return True
-        else:
-            False
 
-    def remove(self, key):
-        hash_value = key %  self.bucket_size
-        self.hash_table[hash_value].remove(key)
+char_hash_table = [0] * 26
+str1 = "abcdabefc"
+for i in str1:
+    hash_value = ord(i)-ord('a')
+    char_hash_table[hash_value] +=1
 
-
-"""
-
-BUCKET = 7
-So, HASH_FUNCTION = x % BUCKET = x % 7
-
-"""
-h = MyHash(7)
-
-h.insert(70)
-h.insert(71)
-h.insert(9)
-h.insert(56)
-h.insert(72)
-print(h.hash_table)
-
-print(h.search(56))       #Output:- True
-
-h.remove(56)
-print(h.hash_table)
-
-print(h.search(56))       #Output:- False
+print(char_hash_table)
