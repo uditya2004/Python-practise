31cd7d30eed4faf71db47387132389ecafefc4b7
Uditya <uditya204@gmail.com>
2026-05-08 18:02:12 +0530

    change

--- Blind 75/3SlidingWindow/2. Longest Substring Without Repeating Characters.py
+++ Blind 75/3SlidingWindow/2. Longest Substring Without Repeating Characters.py
@@ -4,7 +4,7 @@ Given a string s, find the length of the longest substring without repeating cha
 A substring is a contiguous non-empty sequence of characters within a string.
 """
 
-#Using 2 pointers and a list :- Brute Force
+# Method 1:- Using 2 pointers and a list :- Brute Force
 #TC: O(N^2)
 #SC: O(N)
 
@@ -30,61 +30,81 @@ s = "pwwkew"  #Expected output :- 1
 print(lengthOfLongestSubstring(s))
 
 #=========================
-#Using 2 pointers and a hash array + ascii of character :- Brute Force (optimized brute force)
-#TC: O(N^2)
-#SC: O(255) = O(1)
+# Method 2:- Sliding window with set
+"""
+TC:- O(2N)  = O(N)
+    - The right pointer moves from 0 to n-1, visiting each character exactly once.
+    - The left pointer also moves from 0 to at most n-1
 
-def lengthOfLongestSubstring(s):
-    
-    length = 0
-    
+SC:- O(N) => If all characters are unique, the set holds n characters
+"""
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        occur = set()
 
-    for i in range(0, len(s)):
-        hash = [0]*255         # Total possible characters are 255
+        maxLength = 0
+        n = len(s)
 
-        for j in range(i, len(s)):
-            
-            if hash[ord(s[j])] == 1:
-                break
-            
-            else:
-                length = max(length, j-i+1) #Update the length
-                hash[ord(s[j])] = 1
+        left = 0
+        right = 0
+        while left <= right and left <  n and right < n:
+            if s[right] in occur:
+                # move the left pointer till we remove duplicate element from the window
+                while s[right] in occur:
+                    occur.remove(s[left])
+                    left +=1
 
-    return length
+            if s[right] not in occur:
+                occur.add(s[right])    # Adding the s[right] element to set
+                maxLength = max(maxLength, (right - left) + 1)   # Updating the maxlength
+                right +=1   # moving right forwards
+        
+        return maxLength
 
 
-s = "pwwkew"  #Expected output :- 1
-print(lengthOfLongestSubstring(s))
+obj = Solution()
+s = "pwwkew"
+print(obj.lengthOfLongestSubstring(s))
 
 
 #=========================
-#Using 2 pointers Sliding window :- Best Solution
+#Method 3:- Using 2 pointers Sliding window :- Best Solution
+"""
+- We can optimize method 2 by educing the number of operations:
+    - Method 2 may move left one step at a time when a duplicate is found. 
+    - But We can jump directly to the position after the duplicate using a hashmap instead of a set.
+"""
 #TC: O(N)
 #SC: O(N)
 
-def lengthOfLongestSubstring(s):
-    
-    max_len = 0
-    dict1= {}     #SC: O(N)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hash = {}
+        maxlength = 0
+        n = len(s)
 
-    i=0
-    j=i
-    while i<len(s) and j < len(s):
+        left = 0 
+        right = 0
+        while left <= right and left < n and right < n:
+            
+            # If the character is already present in the dictionary and is between left and right , then current element (s[right]) is a duplicate
+            if s[right] in hash and hash[s[right]] >= left:
+                # we move the left pointer ahead of the duplicate element index
+                left = hash[s[right]] + 1
+
+            # Update the last seen index of current character (overwrite)
+            hash[s[right]] = right
+
+            # Update max length
+            maxlength = max(maxlength, (right - left) + 1)
+
+            # move right pointer forward
+            right +=1
         
-        # If the character is already present in the dictionary and is between i and j , then current element (s[j]) is a duplicate and is present already in the previous subarray length. 
-        # So shrink window by moving the i to next of character to remove the duplicate from the wimdow. 
-        if s[j] in dict1:
-            if dict1[s[j]] >= i:
-                i = dict1[s[j]] + 1 # move the i to the next index of of the duplicate element already in the subarray.
-        
+        return maxlength
 
-        # By above "if condition" we made the invalid window valid again, so we can continue with below steps as usual
-        max_len = max(max_len, j-i+1)   # update the max_len
-        dict1[s[j]] = j                 # update the index of character in dictionary
-        j +=1                           # move the pointer j
 
-    return max_len
 
-s = "pwwkew"  
-print(lengthOfLongestSubstring(s))
+obj = Solution()
+s = "abcabcbb"
+print(obj.lengthOfLongestSubstring(s))
